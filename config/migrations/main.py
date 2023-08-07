967e7f968882ce6f8a1756fe4511c8a6dd99f71e
Yuri <yurifc4@gmail.com>
2023-08-07 11:41:42 -0300

    uvicorn

--- config/migrations/main.py
+++ config/migrations/main.py
@@ -1,6 +1,6 @@
 from src.models.users import Users
-
 from config.db.pool import db
 
-with db:
-    db.create_tables([Users])
+def create_tables():
+    with db:
+        db.create_tables([Users])
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import uvicorn, argparse, os
+import uvicorn, argparse, os, multiprocessing
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--env', required=True)
@@ -10,17 +10,22 @@ os.environ['ENV'] = args.env
 ssl_keyfile = None
 ssl_certfile = None
 
+reload = True
+
 if args.env == 'production':
     ssl_keyfile = ''
     ssl_certfile = ''
 
+    reload = False
+
 if __name__ == "__main__":
     uvicorn.run(
-        app='app:asgi',
+        app='src.app:asgi',
         host='0.0.0.0',
-        workers=4 if args.env == 'production' else 1,
         port=6400,
-        reload=args.env == 'development',
+        workers=int(multiprocessing.cpu_count() / 2) + 1,
         ssl_certfile=ssl_certfile,
-        ssl_keyfile=ssl_keyfile
+        ssl_keyfile=ssl_keyfile,
+
+        reload=reload
     )
